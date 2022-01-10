6847988d8161726f3216731cbea03b109763442d
liyunlong <long@123.cn>
2022-01-10 19:37:38 +0800

    first commit

--- src/main/java/com/itheima/HelloGit.java
+++ src/main/java/com/itheima/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git-idea");
         System.out.println("Hello Git-idea2");
+        System.out.println("Hello Git-idea3");
     }
 }
