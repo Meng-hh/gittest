37b06e5c5bb2db24881ca6418dec87ef5b2637a8
liyunlong <long@123.cn>
2022-04-15 11:05:21 +0800

    new commit

--- src/main/java/com/itheima/HelloGit.java
+++ src/main/java/com/itheima/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
         System.out.println("Hello Git-idea4");
         System.out.println("Hello Git-idea5");
         System.out.println("push test");
+        System.out.println("熟悉git");
     }
 }
